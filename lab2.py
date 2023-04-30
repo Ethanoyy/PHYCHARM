ecdfa98de579ecdf97510d70a3bf33d8617fc944
Ethan <ethanyy.23@ichat.sp.edu.sg>
2023-04-30 14:19:48 +0800

    lab2 modified

--- lab2.py
+++ lab2.py
@@ -1,1 +1,20 @@
-print "qwertyuiop"
+
+def CalcuBMI(x, y):
+    print("Height = " + str(x))
+    print("Weight = " + str(y))
+    BMI = (y / (x * x))
+    print("BMI = " + str(BMI))
+
+    if (BMI < 18.5):
+        print("UNDERWEIGHT")
+
+    elif (BMI <= 25.0) and (BMI>=18.5):
+        print("Normal weight")
+
+    else:
+        print("OVERWEIGHT")
+
+CalcuBMI(1.8, 65)
+
+
+
